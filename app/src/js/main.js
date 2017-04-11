f870042596376c4f77299c125345e784fe7a2b9d
Rhonda Impey <rimpey@codefrog.net.au>
2017-04-11 12:12:32 +1000

    Working on Projects Panel
    Challenging to resolve single appearance text for SEO details of the project added to the same column
    of the project containing logo and project summary so that when collapses at mobile size will
    responsively collapse in correct order. However at desktop size this project detail needed to be
    added outside of the row to display correctly.
    
    This was resolved by adding an order flex property of 0 at mobile and 1-9 at desktop.
    Overall the projects panel at desktop is:
    row [ logo-whfnp order-1, proj-whfnp order-4, dtls-whfnp order-7,
          logo-foof order-2, proj-foof order-5, dtls-foof order-8,
          logo-crooked order-3, proj-crooked order-6, dtls-crooked order-9 ]
    
    and for mobile is all set to order-0
    
    Used a media query to set all display:flex order property to order:0 at mobile for default collapse
    Media query sits inside Sass mix-in to loop through and set class .order-#{$i} to property of order-$i

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -1,29 +1,64 @@
-// *** SIDE NAVIGATION ***//
-
-// Set the width of the side navigation to 250px and the left margin of the 
-// page content to 250px can add a black background color to body 
-
-function openNav() {
-    document.getElementById("mySideNav").style.left = "0px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.getElementById("main").style.marginRight = "-250px";
-    // document.body.style.backgroundColor = "rgba(129,129,129,0.4)";
-}
-
-function closeNav() {
-    // style left modified as width wrapped sub menu - not visually pleasing
-    document.getElementById("mySideNav").style.left = "-250px";
-    document.getElementById("main").style.marginLeft= "0";
-    document.getElementById("main").style.marginRight= "0";
-}
+$(document).ready(function() {
 
 
-// *** CONTACT ME FORM ***//
+    // *** SIDE NAVIGATION ***//
 
-// Align bottom of message input to bottom of Email input
-// Get the y-coordinate of the bottom of the phone input
-var emailInput = $('#input-email');
-var bottomOfEmail = emailInput.position().top + emailInput.height();
-// Set the textare height to bottom of phone input - top of textarea
-var messageInput = $('#input-message');
-messageInput.height(bottomOfEmail - messageInput.position().top);
+    // Set the width of the side navigation to 250px and the left margin of the 
+    // page content to 250px can add a black background color to body 
+
+    function openNav() {
+        document.getElementById("mySideNav").style.left = "0px";
+        document.getElementById("main").style.marginLeft = "250px";
+        document.getElementById("main").style.marginRight = "-250px";
+        // document.body.style.backgroundColor = "rgba(129,129,129,0.4)";
+    }
+
+    function closeNav() {
+        // style left modified as width wrapped sub menu - not visually pleasing
+        document.getElementById("mySideNav").style.left = "-250px";
+        document.getElementById("main").style.marginLeft= "0";
+        document.getElementById("main").style.marginRight= "0";
+    }
+
+
+    // *** CONTACT ME FORM ***//
+
+    // Align bottom of message input to bottom of Email input
+    // Get the y-coordinate of the bottom of the phone input
+    var emailInput = $('#input-email');
+    var bottomOfEmail = emailInput.position().top + emailInput.height();
+    // Set the textare height to bottom of phone input - top of textarea
+    var messageInput = $('#input-message');
+    messageInput.height(bottomOfEmail - messageInput.position().top);
+
+
+    // *** PROJECT - READ MORE TOGGLE ***//
+    // $("#whfnp-click").click(function(){
+    //     $("#whfnp-work").toggle();
+    //     // swap label
+    //     label = $(this);
+    //     if (label.html() == "VIEW MORE /") {
+    //         label.html("CLOSE /");
+    //     }else{
+    //         label.html("VIEW MORE /");
+    //     }
+    // });
+
+    $(".toggle-read").click(function(){
+        $('.work-details').hide();
+        $(".toggle-read").html("VIEW MORE /");
+
+        var toggleDiv = $(this).data('id');
+        $("#" + toggleDiv).show();
+        // swap label
+        $(this).html("CLOSE /");
+
+        // label = $(this);
+        // if (label.html() == "VIEW MORE /") {
+        //     label.html("CLOSE /");
+        // }else{
+        //     label.html("VIEW MORE /");
+        // }
+    });
+
+});
