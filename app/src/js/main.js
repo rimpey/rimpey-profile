c4fe2ebe74d67d0d0528531be99ef00b85614f4f
Rhonda Impey <rimpey@codefrog.net.au>
2017-04-16 20:37:44 +1000

    Corrected toggle 'View More' so if user clicks across toggle-reads associated project details are displayed ensuring 'close' is for the current project clicked. Performed an image swap for project with focus a dark/colour logo otherwise with a greyed out logo and text. Resized logo images to be the same.

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -32,38 +32,89 @@ $(document).ready(function() {
     messageInput.height(bottomOfEmail - messageInput.position().top);
 
 
-
+    
     // *** PROJECT - READ MORE TOGGLE and CLICK ON CROSS CLOSE ***//
+
+    //var allWorkDetails = $('.work-details');
 
     $(".toggle-read, .cross-close").on('click', function(e){
 
         e.preventDefault();
+        var workDiv = $('#' + $(this).attr('data-work-id')); 
+        var visible = workDiv.is(':visible');
+
+        // set all text to View More
+        $(".toggle-read").html("VIEW MORE /");
+        // close all project detail divs that are open
+        $(".work-details").removeClass("display-flex"); 
+        // grey text for all projects until selected
+        $(".work-project").removeClass("active-text"); 
+        // get rid of any project logo line hightlights
+        $(".work-logo").removeClass('highlight-line');
+        // set all inactive logos viewable to top
+        //$(".logo-active").removeClass('active-to-top');
+        $(".logo-active").hide();
+        $(".logo-inactive").show();
+
+
+        var logoDiv = $('#' + $(this).attr('data-logo-id'));
+        var workSummary = $('#' + $(this).attr('data-summary-id'));
+        //var workDiv = $('#' + $(this).attr('data-work-id')); 
+
+        // var logoImgActive = logoDiv.find('.logo-active');
+        // var logoImgActive = logoDiv.children('.logo-active');
+        // var logoImgActive = $('.logo-active', logoDiv);
+        // var logoImgActive = logoDiv.find('img').first();
+
+
+
+        // add .active to just clicked 'View More' (not cross clicked) 
+        if ($(this).hasClass("toggle-read") && !visible) {
+
+            logoDiv.addClass('highlight-line'); 
+            //logoImgActive.addClass('active-to-top');
+            logoDiv.find('.logo-active').show();
+            logoDiv.find('.logo-inactive').hide();
+            workSummary.addClass('active-text');
+            workDiv.addClass('display-flex'); 
+            $(this).html("CLOSE /");
+        }
+
+    });
+
+
+    // *** PROJECT - READ MORE TOGGLE and CLICK ON CROSS CLOSE ***//
+
+    // $(".toggle-read, .cross-close").on('click', function(e){
+
+    //     e.preventDefault();
 
         // make all toggle-read's the same to eliminate previously opened label
-        $(".toggle-read").html("VIEW MORE /");
+        // $(".toggle-read").html("VIEW MORE /");
+        // remove all $(".prog-details")
         
         // Display the work details for the work project clicked as a flex
         // var toggleDiv = $(this).data('workId'); // references 'data-work-id' in main.js
-        var workDiv = $('#' + $(this).attr('data-work-id')); // same as above references 'data-work-id' in main.js
+        // var workDiv = $('#' + $(this).attr('data-work-id')); // same as above references 'data-work-id' in main.js
         // alternative to traversing DOM
         // $(this).parent().parent().prev().find('.work-logo').css('border-bottom', '2px solid green');
         // example of referencing another data attribute
-        var logoDiv = $('#' + $(this).attr('data-logo-id'));
+        // var logoDiv = $('#' + $(this).attr('data-logo-id'));
         // display the divide row with border and cross to close
         // var divideDiv = $('#' + $(this).attr('data-divide-id'));
         
-        if (workDiv.is(':visible')) {
+        // if (workDiv.is(':visible')) {
             // close it
-            logoDiv.removeClass('highlight-line');
-            workDiv.removeClass('display-flex');  //.hide(); if not using removeClass()
+            // logoDiv.removeClass('highlight-line');
+            // workDiv.removeClass('display-flex');  //.hide(); if not using removeClass()
             // divideDiv.removeClass('proj-divide');
             // $(".toggle-read").html("VIEW MORE /"); // all toggle-read classes
             // $(this).html("VIEW MORE /"); // toggle-read class clicked
-        } else {
+        // } else {
             // open it
             // Cross Close will never run through this code so safe to change text of $(this) object as can only be from toggle-read
-            logoDiv.addClass('highlight-line'); 
-            workDiv.addClass('display-flex'); // if use .show(); replaces display:flex with display: block
+            // logoDiv.addClass('highlight-line'); 
+            // workDiv.addClass('display-flex'); // if use .show(); replaces display:flex with display: block
             // workDiv.show().css('display', 'flex'); // add/remove class for flex none
             // divideDiv.addClass('proj-divide');
 
@@ -72,7 +123,7 @@ $(document).ready(function() {
             //     $(".cross-close").addClass('cross-right');
             // }
             // swap label for current clicked
-            $(this).html("CLOSE /");
-        }
-    });
+    //         $(this).html("CLOSE /");
+    //     }
+    // });
 });
