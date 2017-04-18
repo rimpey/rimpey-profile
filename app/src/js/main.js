dab7dcb2b78de17ec57fbddf8cc0f8f6429747da
Rhonda Impey <rimpey@codefrog.net.au>
2017-04-18 13:27:33 +1000

    Created logo images for My Projects swap colour/grey scale as click on project to make active/inactive Created RI-round logo to display as smaller mobile sizes rather than full size 'Rhonda Impey' logo Vertical align hamburger Align Meet Me Panel Included links to project websites - foof live - whfnp gh-pages Added some page content for Projects

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -1,3 +1,17 @@
+function openNav() {
+    document.getElementById("mySideNav").style.left = "0px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("main").style.marginRight = "-250px";
+    // document.body.style.backgroundColor = "rgba(129,129,129,0.4)";
+}
+
+function closeNav() {
+    // style left modified as width wrapped sub menu - not visually pleasing
+    document.getElementById("mySideNav").style.left = "-250px";
+    document.getElementById("main").style.marginLeft= "0";
+    document.getElementById("main").style.marginRight= "0";
+}
+
 $(document).ready(function() {
 
 
@@ -6,19 +20,7 @@ $(document).ready(function() {
     // Set the width of the side navigation to 250px and the left margin of the 
     // page content to 250px can add a black background color to body 
 
-    function openNav() {
-        document.getElementById("mySideNav").style.left = "0px";
-        document.getElementById("main").style.marginLeft = "250px";
-        document.getElementById("main").style.marginRight = "-250px";
-        // document.body.style.backgroundColor = "rgba(129,129,129,0.4)";
-    }
-
-    function closeNav() {
-        // style left modified as width wrapped sub menu - not visually pleasing
-        document.getElementById("mySideNav").style.left = "-250px";
-        document.getElementById("main").style.marginLeft= "0";
-        document.getElementById("main").style.marginRight= "0";
-    }
+    
 
 
     // *** CONTACT ME FORM ***//
